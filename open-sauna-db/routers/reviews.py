42f4839b9f16640eb4064dcbe01f54b8c342d4ee
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-09 10:11:37 +0200

    Reviews are accessed in URL through sauna ID, add endpoint for fetching review by review_id

--- open-sauna-db/routers/reviews.py
+++ open-sauna-db/routers/reviews.py
@@ -6,7 +6,7 @@ from db.models import Review
 
 router = APIRouter()
 
-@router.get("/{sauna_id}", response_model=List[ReviewSchema])
+@router.get("/sauna/{sauna_id}", response_model=List[ReviewSchema])
 def get_reviews_for_sauna(sauna_id: str):
     """
     Gets the reviews for given sauna.
@@ -15,8 +15,13 @@ def get_reviews_for_sauna(sauna_id: str):
     reviews = Review.objects(for_sauna=sauna_id)
     return reviews
 
-@router.post("/{sauna_id}", response_model=ReviewSchema, status_code=201)
+@router.post("/sauna/{sauna_id}", response_model=ReviewSchema, status_code=201)
 def create_review_for_sauna(sauna_id: str, review_data: CreateReviewSchema):
     """
     Creates a new review for a given sauna
     """
+
+@router.get("/{review_id}", response_model=ReviewSchema)
+def get_review(review_id: str):
+    # pylint: disable=no-member
+    return Review.objects(review_id=review_id).first()
