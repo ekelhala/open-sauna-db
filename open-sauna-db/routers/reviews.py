a75755e230fb1e10b6bf65c840868036d93cdcb9
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-09 10:08:56 +0200

    Implement get_reviews_for_sauna

--- open-sauna-db/routers/reviews.py
+++ open-sauna-db/routers/reviews.py
@@ -2,14 +2,18 @@ from typing import List
 from fastapi import APIRouter
 
 from routers.schemas import ReviewSchema, CreateReviewSchema
+from db.models import Review
 
 router = APIRouter()
 
 @router.get("/{sauna_id}", response_model=List[ReviewSchema])
-def get_review_for_sauna(sauna_id: str):
+def get_reviews_for_sauna(sauna_id: str):
     """
     Gets the reviews for given sauna.
     """
+    # pylint: disable=no-member
+    reviews = Review.objects(for_sauna=sauna_id)
+    return reviews
 
 @router.post("/{sauna_id}", response_model=ReviewSchema, status_code=201)
 def create_review_for_sauna(sauna_id: str, review_data: CreateReviewSchema):
