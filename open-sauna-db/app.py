bc24eda468df66dccfdb830d079290b63039cf11
Emil Kelhälä <emil.kelhala@gmail.com>
2025-03-02 16:05:33 +0200

    Include auth router

--- open-sauna-db/app.py
+++ open-sauna-db/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import saunas, reviews
+from routers import saunas, reviews, auth
 from db import driver
 
 app = FastAPI()
@@ -7,3 +7,4 @@ driver.init()
 
 app.include_router(router=saunas.router, prefix="/saunas")
 app.include_router(router=reviews.router, prefix="/reviews")
+app.include_router(router=auth.router, prefix="/auth")
